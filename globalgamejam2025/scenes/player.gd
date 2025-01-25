e17fe427a51a8e0e536f113c71234e849246ee0c
David J <74789909+DJ-Jam1@users.noreply.github.com>
2025-01-25 09:48:02 +0100

    basic movement and adding inputs for attack

--- globalgamejam2025/scenes/player.gd
+++ globalgamejam2025/scenes/player.gd
@@ -17,12 +17,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x,input_dir.y ,0 )).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
+		velocity.y = -direction.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED) 
 
 	move_and_slide()
